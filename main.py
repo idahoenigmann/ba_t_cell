4dc2cb6ec3d56ff58a8e052bc8914ff173ada66d
Ida Hoenigmann <idahoney@gmx.at>
2024-05-22 17:44:25 +0200

    testing on sigmoid approx

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def read_data():
     return pd.read_hdf('data/SLB7_231218.h5')
 
 
-def calc_transition_point(w, a, u, k, alpha=0.99):
+def calc_transition_point(w, k, alpha=0.99):
     """
     calculate transition point as point where supremum is almost reached
-    :param w, a, u, k: function parameters
-    :param alpha: margin to supremum
+    :param w: function parameter, middle point of increase
+    :param k: function parameter, steepness of increase
+    :param alpha: margin to supremum, e.g. 0.99 will return the point at which
     :return: transition point
     """
-    tmp = ((1 - alpha) * a) / (alpha * a - u)
+    tmp = 1 / alpha - 1
     if tmp < 0.0001:
         return None
     try:
-        return - 1 / k * math.log(tmp) + w
+        return w - math.log(tmp) / k
     except ValueError:
-        print(f"k: {k}, a: {a}, u: {u}")
-        return - 1 / k * math.log(((1 - alpha) * a) / (alpha * a - u)) + w
+        return None
 
 
 def approximate_with_sigmoid_curve(dataframe):
@@ -41,13 +41,14 @@ def approximate_with_sigmoid_curve(dataframe):
 
     def sigmoid_and_linear_decreasing_(x, w, t, a, d, u, k):
         """
-        w   midpoint of sigmoid function
-        t   transition point between sigmoid and linear function
-        e   end of x-axis
-        a   activated value, supremum of sigmoid function
-        d   decreased value, value reached at end of decrease
-        u   unactivated value, infimum of sigmoid function
-        k   steepness of sigmoid function
+        piecewise function, logistic function followed by linear function
+        :param x: point at which to evaluate the function
+        :param w: midpoint of sigmoid function
+        :param t: transition point between sigmoid and linear function
+        :param a: activated value, supremum of sigmoid function
+        :param d: decreased value, value reached at end of decrease
+        :param u: unactivated value, infimum of sigmoid function
+        :param k: steepness of sigmoid function
         """
         if t is None:  # transition point lies outside datapoints (flat left side)
             return u
@@ -65,8 +66,14 @@ def approximate_with_sigmoid_curve(dataframe):
     def sigmoid_and_linear_decreasing(x_arr, w, a, d, u, k):
         """
         wrapper for sigmoid_and_linear_decreasing_ function, takes list as input
+        :param x_arr: list of points at which to evaluate the function
+        :param w: midpoint of sigmoid function
+        :param a: activated value, supremum of sigmoid function
+        :param d: decreased value, value reached at end of decrease
+        :param u: unactivated value, infimum of sigmoid function
+        :param k: steepness of sigmoid function
         """
-        t = calc_transition_point(w, a, u, k)
+        t = calc_transition_point(w, k)
         return np.vectorize(sigmoid_and_linear_decreasing_)(x_arr, w, t, a, d, u, k)
 
     min_val, median_val, max_val = np.min(dataframe['ratio']), np.median(dataframe['ratio']), np.max(dataframe['ratio'])
@@ -80,56 +87,20 @@ def approximate_with_sigmoid_curve(dataframe):
     dataframe['fit_sigmoid'] = sigmoid_and_linear_decreasing(dataframe['frame'], *popt)
 
     w, a, d, u, k = popt
-    t = calc_transition_point(w, a, u, k)
+    t = calc_transition_point(w, k)
     return {'w': w, 't': t, 'e': end, 'a': a, 'd': d, 'u': u, 'k': k}
 
 
-def approximate_residuum_with_sin(dataframe, start, end):
+def approximate_residuum_with_fft(dataframe, number_of_frequencies_kept, start, end):
     """
-    approximates the residuum by a sin function with fluctuating amplitude
+    approximates the residuum between frames start and end by sin generated with fft
     changes dataframe! adds a column named fit_sin
     :param dataframe: datapoints to approximate, must contain columns ratio and fit_sigmoid
-    :returns: parameters for sin
-    """
-
-    def sin_with_changing_amplitude_(x, f, phi, a_0):
-        """
-        f     frequency
-        phi   phase
-        a_i   coefficients of amplitude polynomial of degree 0
-        """
-        return (a_0) * math.sin(2 * math.pi * f * x + phi)
-
-    def sin_with_changing_amplitude(x_arr, f, phi, a_0):
-        """
-        wrapper for sin_with_changing_amplitude_ function, takes list as input
-        """
-        return np.vectorize(sin_with_changing_amplitude_)(x_arr, f, phi, a_0)
-
-    lower_bounds = (0.001, 0, -np.inf)
-    upper_bounds = (0.1, 2 * math.pi, np.inf)
-    p0 = (0.01, 0, 0)
-
-    popt, *_ = scipy.optimize.curve_fit(sin_with_changing_amplitude, dataframe['frame'][start:end],
-                                        dataframe['residuum'][start:end], p0=p0,
-                                        method='trf', bounds=(lower_bounds, upper_bounds))
-
-    dataframe['fit_sin'] = np.concatenate(
-        (np.zeros(start), sin_with_changing_amplitude(dataframe['frame'][start:end], *p0)))
-
-    f, phi, a_0 = popt
-    return {'f': f, 'phi': phi, 'a_0': a_0}
-
-
-def approximate_residuum_with_fft(dataframe, start, end):
-    """
-    approximates the residuum by sin generated with fft
-    changes dataframe! adds a column named fit_sin
-    :param dataframe: datapoints to approximate, must contain columns ratio and fit_sigmoid
+    :param number_of_frequencies_kept: how many frequencies are used in the approximation
+    :param start: the point from which the approximation with periodic functions is to start
+    :param end: the point from which the approximation with periodic functions is to end
     :returns: parameters for fft
     """
-
-    number_of_frequencies_kept = 10
 
     # assumes frames are evenly spaced!
     fft_out = np.fft.fft(dataframe['residuum'][start:end])
@@ -176,8 +147,16 @@ def calc_residuum_and_error(dataframe):
     return np.dot(dataframe['residuum'], dataframe['residuum']) / len(dataframe['residuum'])
 
 
-if __name__ == '__main__':
-    matplotlib.use('TkAgg')
+def all_particles_to_parameters(min_length_recording, output_information=True, visualize_particles=False):
+    """
+    generates the parameters for all particles of the dataset
+    :param min_length_recording: all particles which were recorded for less than this number of frames will be skipped
+    :param output_information: if True information such as resulting parameters and error will be printed
+    :param visualize_particles: if True a plot showing the data as well as approximation of each particle is shown
+    :return: dictionary where keys are particle indices and values are corresponding parameter dictionaries
+    """
+    if visualize_particles:
+        matplotlib.use('TkAgg')
     data = read_data()
 
     all_parameters = dict()
@@ -187,50 +166,46 @@ if __name__ == '__main__':
         single_particle_data = data.loc[data['particle'] == particle_idx][['frame', 'ratio']]
 
         # skip if too few datapoints
-        if len(single_particle_data['frame']) < 20:
+        if len(single_particle_data['frame']) < min_length_recording:
             continue
 
         # might throw error if best fit was not found within limited number of tries
         try:
             parameters_sigmoid = approximate_with_sigmoid_curve(single_particle_data)
         except RuntimeError as e:
-            print(e)
+            if output_information:
+                print(e)
             continue
 
         rel_error_sigmoid = calc_residuum_and_error(single_particle_data)
 
+        """
         # calculate the point at which the transition between sigmoid and linear function
         if parameters_sigmoid['t'] is None:
             transition_index = single_particle_data['frame'][-1]
         else:
             transition_index = int((np.abs(single_particle_data['frame'] - parameters_sigmoid['t'])).argmin())
 
-        # use optimize to fit sin
-        # parameters_sin = approximate_residuum_with_sin(single_particle_data, transition_index,
-        #                                                len(single_particle_data['frame']))
-
         # use fft to fit sin
-        parameters_sin = approximate_residuum_with_fft(single_particle_data, transition_index,
+        parameters_sin = approximate_residuum_with_fft(single_particle_data, 10, transition_index,
                                                        len(single_particle_data['frame']))
 
         parameters = {**parameters_sigmoid, **parameters_sin}
         single_particle_data['fit_total'] = single_particle_data['fit_sigmoid'] + single_particle_data['fit_sin']
-        rel_error_total = calc_residuum_and_error(single_particle_data)
+        rel_error_total = calc_residuum_and_error(single_particle_data)"""
 
+
+        parameters, rel_error_total = parameters_sigmoid, 0
         all_parameters[particle_idx] = parameters
 
-        print(f"parameters: {parameters}")
-        print(f"mse sigmoid: {rel_error_sigmoid}, mse total: {rel_error_total}")
-        # visualize(single_particle_data)
+        if output_information:
+            print(f"parameters: {parameters}")
+            print(f"mse sigmoid: {rel_error_sigmoid}, mse total: {rel_error_total}")
+        if visualize_particles:
+            visualize(single_particle_data)
 
-    # statistic evaluation
-    statistics = {"steepness": [all_parameters[idx]['k'] for idx in all_parameters.keys()],
-                  "start_increase": [calc_transition_point(all_parameters[idx]['w'], all_parameters[idx]['a'],
-                                                           all_parameters[idx]['u'], all_parameters[idx]['k'], 0.01)
-                                     for idx in all_parameters.keys() if calc_transition_point(all_parameters[idx]['w'], all_parameters[idx]['a'],
-                                                           all_parameters[idx]['u'], all_parameters[idx]['k'], 0.01) is not None],
-                  "end_increase": [all_parameters[idx]['t'] for idx in all_parameters.keys() if all_parameters[idx]['t'] is not None]}
+    return all_parameters
 
-    plt.boxplot(statistics["end_increase"])
-    plt.show()
 
+if __name__ == '__main__':
+    all_parameters = all_particles_to_parameters(20, visualize_particles=True)
